2f90efd50a27bfcdccb8703a907bfa7e1dda19b2
Anjali Vijay <anjali.vijay@qed42.com>
2019-08-12 12:32:39 +0530

    Mobile theming fixed for header and js file added for hamburger.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-(function global() {
-  // console.log('JS');
-})();
+(function global($) {
+  $('.header__menu--mobile ul').css('display', 'none');
+  $('.header__menu--mobile .hamburger').click(function() {
+    if ($('.header__menu--mobile').hasClass('hamburger-open')) {
+      $('.header__menu--mobile ul').css('display', 'none');
+      $('.header__menu--mobile').removeClass('hamburger-open');
+    } else {
+      $('.header__menu--mobile ul').css('display', 'block');
+      $('.header__menu--mobile').addClass('hamburger-open');
+    }
+  });
+})(jQuery);
